074ab02272cc36c06df04923cef9c7fbd31d72a2
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-09 09:14:05 -0700

    Fix form so that it can be used in Users
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

--- front-end/src/components/UserForm.js
+++ front-end/src/components/UserForm.js
@@ -1,28 +1,45 @@
 import React, { useState } from "react"
 
 const UserForm = ({ user, onSubmit }) => {
-    const [{ name, bio }, setUser] = useState({
-        name: user.name,
-        bio: user.bio
-    })
+    const [name, setName] = useState(user ? user.name : "")
+    const [bio, setBio] = useState(user ? user.bio : "")
 
     const handleSubmit = e => {
         e.preventDefault()
         onSubmit({ name, bio })
+        setName("")
+        setBio("")
     }
 
     const handleChange = e => {
-        setUser({ ...user, [e.target.name]: e.target.value })
+        const value = e.target.value
+        switch (e.target.name) {
+            case "name":
+                setName(value)
+                break
+            case "bio":
+                setBio(value)
+            default:
+                break
+        }
     }
+
     return (
         <form>
             <input
                 type="text"
+                placeholder="What's the user's name?"
                 name="name"
                 value={name}
                 onChange={handleChange}
             />
-            <input type="text" name="bio" value={bio} onChange={handleChange} />
+            <input
+                type="text"
+                placeholder="What's the user's bio?"
+                name="bio"
+                value={bio}
+                onChange={handleChange}
+            />
             <button onClick={handleSubmit}>Done</button>
         </form>
     )
