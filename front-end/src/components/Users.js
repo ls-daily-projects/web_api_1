969051ef713a6fa84ffbcbe0c78c9c244d1a1387
Mikis Woodwinter <mikis.woodwinter@gmail.com>
2019-07-09 09:14:34 -0700

    Add form to Users component
    Signed-off-by: Mikis Woodwinter <mikis.woodwinter@gmail.com>

--- front-end/src/components/Users.js
+++ front-end/src/components/Users.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react"
 import axios from "axios"
 
 import User from "./User"
+import UserForm from "./UserForm"
 import LoadingIndicator from "./LoadingIndicator"
 
 const Users = () => {
@@ -22,12 +23,22 @@ const Users = () => {
         localStorage.setItem("users", JSON.stringify(users))
     }, [users])
 
+    const handleSubmit = async user => {
+        try {
+            const { data } = await axios.post("/api/users", user)
+            setUsers([...users, data])
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
     if (isLoading) {
         return <LoadingIndicator />
     }
     return (
         <>
             <h1>Users</h1>
+            <UserForm onSubmit={handleSubmit} />
             <ul>
                 {users.map(user => (
                     <User key={user.id} {...user} />
